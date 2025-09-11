10e4c4b15757c42a8abfa07b89032e4d57ab39a1
Pravin-Choudhary <pravinchoudhary.aesxa29@gmail.com>
2025-09-12 02:08:49 +0530

    update notfound.tsx

--- Client/pages/NotFound.tsx
+++ Client/pages/NotFound.tsx
@@ -1,27 +1,22 @@
-import { useLocation } from "react-router-dom";
-import { useEffect } from "react";
+"use client";
+import { usePathname } from "next/navigation";
 
-const NotFound = () => {
-  const location = useLocation();
+export default function NotFound() {
+  const pathname = usePathname();
 
-  useEffect(() => {
-    console.error(
-      "404 Error: User attempted to access non-existent route:",
-      location.pathname
-    );
-  }, [location.pathname]);
+  console.error("404 Error: Missing route:", pathname);
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="text-center">
         <h1 className="text-4xl font-bold mb-4">404</h1>
-        <p className="text-xl text-gray-600 mb-4">Oops! Page not found</p>
+        <p className="text-xl text-gray-600 mb-4">
+          Oops! Page <code>{pathname}</code> not found
+        </p>
         <a href="/" className="text-blue-500 hover:text-blue-700 underline">
           Return to Home
         </a>
       </div>
     </div>
   );
-};
-
-export default NotFound;
+}
